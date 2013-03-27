6ece558e5eded77f99dfe2e0d2196b3435a908c5
Jean-R?my Falleri <jr.falleri@laposte.net>
2013-03-27 18:01:29 +0100

    updated data.

--- commit-migration/src/main/java/fr/labri/harmony/analysis/migration/scanlib/ComputeGoogleRules.java
+++ commit-migration/src/main/java/fr/labri/harmony/analysis/migration/scanlib/ComputeGoogleRules.java
@@ -2,7 +2,6 @@ package fr.labri.harmony.analysis.migration.scanlib;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -15,6 +14,11 @@ public class ComputeGoogleRules {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		ObjectMapper map = new ObjectMapper();
 		ArrayNode rules = (ArrayNode) map.readTree(new File("data/MigrationRules.json"));
+		
+		double total = 0;
+		double right = 0;
+		double alg = 0;
+
 		for (int i = 0; i < rules.size(); i++) {
 			JsonNode rule = rules.get(i);
 			String source = rule.get("source").asText();
@@ -29,26 +33,38 @@ public class ComputeGoogleRules {
 					String[] texts = google.get(j).asText().toLowerCase().split("\\.\\.\\.");
 					for (String text: texts) if (isMigration(text, source, target)) occur++;
 				}
-				if ( occur > 0 ) found = true;
+				if (occur > 0) found = true;
+				
+				if (correct) total++;
+				if (found) alg++;
+				if (found == correct && found == true) right++;
+				
+				if (!found && correct) System.out.println(source + " " + target);
 			}
-			if (found == true) System.out.println(source + " - " + target + " - " + correct  + " - " + found);
 		}
+		
+		System.out.println("Precision " + right / alg);
+		System.out.println("Recall " + right / total);
 	}
 	
 	public static boolean isMigration(String text, String source, String target) {
 		for (String regexp: regexps(source, target)) if (text.matches(regexp)) {
-			System.out.println(text);
-			System.out.println(Arrays.toString(regexps(source, target)));
+			//System.out.println(text);
+			//System.out.println(Arrays.toString(regexps(source, target)));
 			return true;
 		}
 		return false;
 	}
 	
 	public static String[] regexps(String source, String target) {
-		return new String[] {
+		/* return new String[] {
 			".*migrat.*" + source + ".*\\s.*" + target + ".*",
 			".*replac.*" + source + ".*\\s.*" + target + ".*",
 			".*from.*" + source + ".*to.*" + target + ".*",
+		};
+		*/
+		return new String[] {
+				".*" + source + ".*" + target + ".*"
 		};
 	}
 
